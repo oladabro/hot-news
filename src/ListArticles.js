7a15955a5d05b3131443b43b31b701685f9aad9b
Ola <oladabro@gmail.com>
2021-03-03 21:19:47 +0100

    install axios

--- src/ListArticles.js
+++ src/ListArticles.js
@@ -1,22 +1,43 @@
-const ListArticles = () => {
-  // fetch data
-  const getData = async () => {
-    const response = await fetch(
-      'https://community-hacker-news-v1.p.rapidapi.com/topstories.json?print=pretty',
-      {
-        method: 'GET',
-        headers: {
-          'x-rapidapi-key':
-            '2dad272f2bmsh218b44d9b7874a6p1bcef1jsn0a61d3a434a6',
-          'x-rapidapi-host': 'community-hacker-news-v1.p.rapidapi.com',
-        },
-      }
-    );
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-    const data = await response.json();
-    console.log(data[0]);
-  };
-  getData();
+const ListArticles = () => {
+  const [articles, setArticles] = useState([]);
+
+  // fetch data
+
+  useEffect(() => {
+    console.log('chce załadować dane');
+  }, []);
+  // const getData = async () => {
+  //   const response = await fetch(
+  //     'https://newsapi.org/v2/top-headlines?country=us&apiKey=ae78270a03ee4563909ff5b718228bd7',
+  //     // 'https://newsapi.org/v2/everything?q=morawiecki&apiKey=ae78270a03ee4563909ff5b718228bd7',
+  //     {
+  //       method: 'GET',
+  //       // headers: {
+  //       //   'x-rapidapi-key':
+  //       //     '2dad272f2bmsh218b44d9b7874a6p1bcef1jsn0a61d3a434a6',
+  //       //   'x-rapidapi-host': 'community-hacker-news-v1.p.rapidapi.com',
+  //       // },
+  //     }
+  //   );
+
+  //   const data = await response.json();
+  //   // setArticles(data);
+  //   console.log(response);
+  // };
+
+  // getData().then((data) => {
+  //   console.log(data.articles);
+  //   let template = '';
+
+  //   data.map((article) => {
+  //     template = `<div>${article.author}</div>`;
+  //   });
+  // });
+
   return <div>Here will be list of articles</div>;
 };
 
